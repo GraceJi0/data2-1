ea96d048df7bd6a7d9084ed8b6332a30d9c54cc8
GraceJi0 <jid@myumanitoba.ca>
2018-01-25 15:01:41 -0600

    create the project

---
+++ FileTableModel.java
@@ -0,0 +1,80 @@
+package project;
+
+import java.io.File;
+import java.util.Date;
+import javax.swing.ImageIcon;
+import javax.swing.filechooser.FileSystemView;
+import javax.swing.table.AbstractTableModel;
+
+/** A TableModel to hold File[]. */
+public class FileTableModel extends AbstractTableModel 
+{
+    
+    private File[] files;
+    private FileSystemView fileSystemView = FileSystemView.getFileSystemView();
+    private String[] columns = {"Icon","File"};
+    
+    FileTableModel() 
+    {
+        this(new File[0]);
+    }
+    
+    FileTableModel(File[] files) 
+    {
+        this.files = files;
+    }
+    
+    public Object getValueAt(int row, int column) 
+    {
+        File file = files[row];
+        switch (column) 
+        {
+            case 0:
+                return fileSystemView.getSystemIcon(file);
+            case 1:
+                return fileSystemView.getSystemDisplayName(file);
+            default:
+                System.err.println("Logic Error");
+        }
+        return "";
+    }
+    
+    public int getColumnCount() 
+    {
+        return columns.length;
+    }
+    
+    public Class<?> getColumnClass(int column) 
+    {
+        if(column == 0)
+        {
+            return ImageIcon.class;
+        }
+        else
+        {
+            return String.class;
+        }
+        
+    }
+    
+    public String getColumnName(int column) 
+    {
+        return columns[column];
+    }
+    
+    public int getRowCount() 
+    {
+        return files.length;
+    }
+    
+    public File getFile(int row) 
+    {
+        return files[row];
+    }
+    
+    public void setFiles(File[] files) 
+    {
+        this.files = files;
+        fireTableDataChanged();
+    }
+}
---
+++ Interface.java
@@ -0,0 +1,45 @@
+package project;
+import java.awt.Dimension;
+import java.io.File;
+
+import javax.swing.*;
+
+public class Interface 
+{
+    private static void showGUI() 
+    { 
+        //00003932-S_File_4_mtDNA_partitions_for_RAxML
+        //aaa
+        /*JFrame f = new InterfaceMain(new File("/Users/dinghanji/Desktop/output/aaa.txt")).getMainFrame();
+         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         f.pack();
+         f.setMinimumSize(f.getSize());
+         f.setVisible(true);*/
+        
+        
+        JFrame f = new JFrame("Data Management");
+        f.setPreferredSize(new Dimension(900, 700));
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        InterfaceDirectories fileBrowser = new InterfaceDirectories();
+        f.setContentPane(fileBrowser.getGui());
+        
+        f.pack();
+        f.setLocationByPlatform(true);
+        f.setMinimumSize(f.getSize());
+        f.setVisible(true);
+        
+        fileBrowser.showRootFile();
+    }
+    
+    public static void main(String[] args) 
+    {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() 
+                                                   {
+            public void run() 
+            {
+                showGUI();
+            }
+        });
+    }
+}
