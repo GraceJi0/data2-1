78990fad03c4c0500c63cb4526b8e9d69f53cedb
GraceJi0 <jid@myumanitoba.ca>
2018-02-23 11:26:15 -0600

    .

--- data/src/project/ExtensionFileFilter.java
+++ data/src/project/ExtensionFileFilter.java
@@ -4,43 +4,57 @@ import java.io.File;
 
 import javax.swing.filechooser.FileFilter;
 
-class ExtensionFileFilter extends FileFilter {
+class ExtensionFileFilter extends FileFilter 
+{
 	  String description;
 
 	  String extensions[];
 
-	  public ExtensionFileFilter(String description, String extension) {
+	  public ExtensionFileFilter(String description, String extension) 
+	  {
 	    this(description, new String[] { extension });
 	  }
 
-	  public ExtensionFileFilter(String description, String extensions[]) {
-	    if (description == null) {
+	  public ExtensionFileFilter(String description, String extensions[]) 
+	  {
+	    if (description == null) 
+	    {
 	      this.description = extensions[0];
-	    } else {
+	    } else 
+	    {
 	      this.description = description;
 	    }
 	    this.extensions = (String[]) extensions.clone();
 	    toLower(this.extensions);
 	  }
 
-	  private void toLower(String array[]) {
-	    for (int i = 0, n = array.length; i < n; i++) {
+	  private void toLower(String array[])
+	  {
+	    for (int i = 0, n = array.length; i < n; i++) 
+	    {
 	      array[i] = array[i].toLowerCase();
 	    }
 	  }
 
-	  public String getDescription() {
+	  public String getDescription() 
+	  {
 	    return description;
 	  }
 
-	  public boolean accept(File file) {
-	    if (file.isDirectory()) {
+	  public boolean accept(File file) 
+	  {
+	    if (file.isDirectory()) 
+	    {
 	      return true;
-	    } else {
+	    } 
+	    else 
+	    {
 	      String path = file.getAbsolutePath().toLowerCase();
-	      for (int i = 0, n = extensions.length; i < n; i++) {
+	      for (int i = 0, n = extensions.length; i < n; i++) 
+	      {
 	        String extension = extensions[i];
-	        if ((path.endsWith(extension) && (path.charAt(path.length() - extension.length() - 1)) == '.')) {
+	        if ((path.endsWith(extension) && (path.charAt(path.length() - extension.length() - 1)) == '.')) 
+	        {
 	          return true;
 	        }
 	      }
