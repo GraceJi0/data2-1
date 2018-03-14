ec1bfd1d062045a7322e789f0564ba89722e538d
GraceJi0 <jid@myumanitoba.ca>
2018-03-14 16:27:37 -0500

    test: use thread to wait for 5sec to see if the file needs longer to open or not

--- data/src/project/RunAllFiles.java
+++ data/src/project/RunAllFiles.java
@@ -14,6 +14,7 @@ public class RunAllFiles {
 	{
 		result = "";
 		File file = new File("/Users/dinghanji/Desktop/projectFile/noncoop_ex");
+		//run(file);
 		runFiles(file);
 		System.out.println("end");
 	}
@@ -35,24 +36,28 @@ public class RunAllFiles {
 		                					document.getName().contains("xlsx")||document.getName().contains("xls"))
 		                					&& (!document.getName().contains("metaData") && !document.getName().contains("README") && document.getName().contains(".")))
 		                		{		
-		                				//System.out.println(fileName+"    ---");
-		                				RunableThread t2 = new RunableThread("run_files",document);
-		                				t2.start();
-		                				try 
-		                				{
-											Thread.sleep(5000);
-									} 
-		                				catch (InterruptedException e) 
-		                				{
-											e.printStackTrace();
-									}
-		                				if(t2.t.isAlive())
-		                				{
-		                					t2.change();
-		                					t2.stopThread();
-		                				}
-		                				System.out.println(t2.getInformation());
-				                		
+		                			RunableThread t2 = new RunableThread("run_files",document);
+		                			t2.start();
+		                			try 
+		                			{
+									t2.t.join(5000);
+								} 
+		                			catch (InterruptedException e)
+		                			{
+									e.printStackTrace();
+								}
+		                			//System.out.println(fileName);
+		            				if(t2.t.isAlive())
+			            			{
+			                				t2.change();
+			                				System.out.println(t2.getInformation());
+			                				t2.interrupt();             					
+			                		}
+		            				else
+		            				{
+		            					System.out.println(t2.getInformation());
+		            				}
+		                				
 		            			}
 		                	}
 	                		
@@ -61,58 +66,7 @@ public class RunAllFiles {
 	     } 
 	  }
 	
-	/* public void run(File file)
-	 {
-		 if (file != null && file.isDirectory()) 
-	     {
-	            File[] files = file.listFiles();
-	            for (File child : files) 
-	            {
-	                if (child.isDirectory()) 
-	                {
-	                		if(!child.getName().equals("Geospiza") && !child.getName().equals("Hirundo%20rustica") )
-	                		{
-		                		File[] childrenfiles = child.listFiles();
-		                		for(File document : childrenfiles)
-		                		{
-		                			
-		                			String information = "";
-		                			String fileName = document.getName();
-		                			if((document.getName().contains("txt")||document.getName().contains("csv")||
-		                					document.getName().contains("xlsx")||document.getName().contains("xls"))
-		                					&& (!document.getName().contains("metaData") && !document.getName().contains("README") && document.getName().contains(".")))
-		                			{
-		                				information+=fileName+"\n";
-		                				if(fileName.equals("00004340-83Hapmap_simple2_filtered.xlsx") 
-		                						|| fileName.equals("00001958-2016_06_27 MalesSpecMorph_museum.xlsx")
-		                						|| fileName.equals("00003053-GeneticsPedigree_Publication.xlsx")
-		                						|| fileName.equals("00003349-data_genotypes.csv")
-		                						|| fileName.equals("00004815-UK_genotypes_PED.txt"))
-		                				{
-		                					information+= "Time: too long\n";
-				                			information+="Size: "+document.length()+"\n\n";
-		                				}
-		                				else
-		                				{
-		                					System.out.println(fileName+"    ---");
-				                			long startTime = System.nanoTime();
-				                			EditFile editFile = new EditFile( document);
-				                			long endTime   = System.nanoTime();
-				                			long totalTime = endTime - startTime;
-				                			double seconds = (double)totalTime/1000000000.0;
-				                			information+= "Time: "+seconds+"\n";
-				                			information+="Size: "+document.length()+"\n\n";
-		                				}
-		                				result+=information;
-			                			System.out.println(information);
-		                			}
-		                		}
-	                		}
-	                }
-	            }
-	     }
-		 
-	  }*/
+	 
 }
 
 class RunableThread implements Runnable
@@ -122,6 +76,7 @@ class RunableThread implements Runnable
 	private String fileName;
 	private File document;
 	private String information;
+	EditFile editFile;
 	
 	public RunableThread(String newName, File file)
 	{
@@ -133,24 +88,25 @@ class RunableThread implements Runnable
 	@Override
 	public void run() 
 	{
-		information ="";
-		information = fileName+"\n";
-		long startTime = System.nanoTime();
-		EditFile editFile = new EditFile( document);
-		long endTime   = System.nanoTime();
-		long totalTime = endTime - startTime;
-		double seconds = (double)totalTime/1000000000.0;
-		information+= "Time: "+seconds+"\n";
-		information+="Size: "+document.length()+"\n\n";
+			information ="";
+			information = fileName+"\n";
+			long startTime = System.nanoTime();
+			 editFile = new EditFile( document);
+			long endTime   = System.nanoTime();
+			long totalTime = endTime - startTime;
+			double seconds = (double)totalTime/1000000000.0;
+			information+= "Time: "+seconds+"\n";
+			information+="Size: "+document.length()+"\n\n";
 	}
 	
 	public void change()
 	{
+		information = fileName+"\n";
 		information+= "Time: too long\n";
 		information+="Size: "+document.length()+"\n\n";
 	}
 	
-	public void stopThread()
+	public void interrupt()
 	{
 		t.interrupt();
 	}
@@ -168,5 +124,5 @@ class RunableThread implements Runnable
 	         t.start ();
 	    }
 	}
-	
 }
+
